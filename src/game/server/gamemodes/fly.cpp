adda869a204892a1a547ac6854dcb723011c93cb
ChillerDragon <ChillerDragon@gmail.com>
2020-04-16 20:37:08 +0200

    Initial fly commit, gametype rename

--- src/game/server/gamemodes/fly.cpp
+++ src/game/server/gamemodes/fly.cpp
@@ -1,18 +1,18 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include "mod.h"
+#include "fly.h"
 
-CGameControllerMOD::CGameControllerMOD(class CGameContext *pGameServer)
+CGameControllerFLY::CGameControllerFLY(class CGameContext *pGameServer)
 : IGameController(pGameServer)
 {
 	// Exchange this to a string that identifies your game mode.
 	// DM, TDM and CTF are reserved for teeworlds original modes.
-	m_pGameType = "MOD";
+	m_pGameType = "fly";
 
 	//m_GameFlags = GAMEFLAG_TEAMS; // GAMEFLAG_TEAMS makes it a two-team gamemode
 }
 
-void CGameControllerMOD::Tick()
+void CGameControllerFLY::Tick()
 {
 	// this is the main part of the gamemode, this function is run every tick
 
--- src/game/server/gamemodes/fly.h
+++ src/game/server/gamemodes/fly.h
@@ -6,10 +6,10 @@
 
 // you can subclass GAMECONTROLLER_CTF, GAMECONTROLLER_TDM etc if you want
 // todo a modification with their base as well.
-class CGameControllerMOD : public IGameController
+class CGameControllerFLY : public IGameController
 {
 public:
-	CGameControllerMOD(class CGameContext *pGameServer);
+	CGameControllerFLY(class CGameContext *pGameServer);
 	virtual void Tick();
 	// add more virtual functions here if you wish
 };
